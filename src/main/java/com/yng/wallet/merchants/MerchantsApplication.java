0713f87571be3fa58ee44e7218b7f353dd9ac68f
yngyuan <zedd.yuan@utexas.edu>
2020-05-18 22:09:55 -0500

    Initalize Project

---
+++ src/main/java/com/yng/wallet/merchants/MerchantsApplication.java
@@ -0,0 +1,14 @@
+package com.yng.wallet.merchants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class MerchantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yng/wallet/merchants/MerchantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yng.wallet.merchants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
